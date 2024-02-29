c85a565482c81d5acf5a5133361e8a5d1842a776
alik-balika <balikaalik@gmail.com>
2024-02-28 17:03:53 -0800

    create PageHeader

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { CssBaseline } from "@mui/material";
+import PageHeader from "./components/PageHeader";
+import PageContent from "./components/PageContent";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <CssBaseline />
+      <PageHeader />
+      <PageContent />
+    </div>
+  );
 };
 
 export default App;
--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AppBar, Toolbar, Typography, useTheme } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const PageHeader = () => {
-  return <div>PageHeader</div>;
+  const [src, setSRC] = useState(
+    "https://user-images.githubusercontent.com/19292210/60553863-044dd200-9cea-11e9-987e-7db84449f215.png"
+  );
+
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
+
+  useEffect(() => {
+    if (isSmallScreen) {
+      setSRC(
+        "https://user-images.githubusercontent.com/19292210/60553865-044dd200-9cea-11e9-859c-d6f266e2f01f.png"
+      );
+    } else {
+      setSRC(
+        "https://user-images.githubusercontent.com/19292210/60553863-044dd200-9cea-11e9-987e-7db84449f215.png"
+      );
+    }
+  }, [isSmallScreen]);
+
+  return (
+    <div>
+      <AppBar position="static" sx={{ backgroundColor: "white" }}>
+        <Toolbar>
+          <GitHubIcon fontSize="large" sx={{ mr: 1, color: "black" }} />
+          <Typography variant="h4" color="black">
+            Github Status
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <img
+        src={src}
+        alt="Github header"
+        style={{
+          height: "auto",
+          width: "100%",
+        }}
+      />
+    </div>
+  );
 };
 
 export default PageHeader;
