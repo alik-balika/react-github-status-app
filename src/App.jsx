dc8ae769527b205429933c26c07c735914217d64
alik-balika <balikaalik@gmail.com>
2024-02-28 16:36:55 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
+// https://user-images.githubusercontent.com/19292210/60553865-044dd200-9cea-11e9-859c-d6f266e2f01f.png
